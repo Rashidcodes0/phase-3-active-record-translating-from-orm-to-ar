bb109a777babe66e4a5fa358200d2ffd041ccbf4
Abdirashidnoor <abdirashid16112@gmail.com>
2023-06-08 12:31:32 +0300

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,15 @@
-class Dog
+require 'active_record'
+
+class Dog < ActiveRecord::Base
+  # Assuming the database table is already created with appropriate columns (id, name, breed)
+  # and the ActiveRecord connection is properly established
+
+  def self.find_by_name(name)
+    where(name: name).first
+  end
+
+  def self.find_by_id(id)
+    find_by(id: id)
+  end
 end
+
